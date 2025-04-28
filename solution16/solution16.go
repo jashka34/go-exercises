ee61d120902bd4a8ec60a586c05aa6cce1a62ae4
jashka34 <a_cherniadiev@mail.ru>
2025-04-28 06:52:04 +0300

    16/50 begin

---
+++ solution16/solution16.go
@@ -0,0 +1,6 @@
+package solution16
+
+func Map(strs []string, mapFunc func(s string) string) []string {
+	var ret []string
+	return ret
+}
---
+++ solution16/solution16_test.go
@@ -0,0 +1,29 @@
+package solution16
+
+import (
+	"strings"
+	"testing"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMap(t *testing.T) {
+	a := assert.New(t)
+	testMap(a, []string{"John", "Peter", "Fedor"}, []string{"john", "peter", "fedor"}, func(s string) string {
+		return cases.Title(language.English).String(s)
+	})
+	testMap(a, []string{"hello", "world"}, []string{"HELLO", "WORLD"}, func(s string) string {
+		return strings.ToLower(s)
+	})
+}
+
+func testMap(a *assert.Assertions, expected, input []string, mapFunc func(s string) string) {
+	inputCopy := make([]string, len(input))
+	copy(inputCopy, input)
+
+	a.Equal(expected, Map(input, mapFunc))
+	a.Equal(inputCopy, input) // check that the initial slice hasn't been modified.
+}

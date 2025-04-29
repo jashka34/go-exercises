0dcf444219cee67f4af022a1052ceb03f136c269
jashka34 <a_cherniadiev@mail.ru>
2025-04-29 05:57:40 +0300

    16/50

--- solution16/solution16.go
+++ solution16/solution16.go
@@ -2,5 +2,8 @@ package solution16
 
 func Map(strs []string, mapFunc func(s string) string) []string {
 	var ret []string
+	for _, s := range strs {
+		ret = append(ret, mapFunc(string(s)))
+	}
 	return ret
 }

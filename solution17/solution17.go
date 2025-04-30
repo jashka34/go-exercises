1c25ffc405110c116d7a2f2b3084d49a09ae2d9f
jashka34 <a_cherniadiev@mail.ru>
2025-05-01 00:42:32 +0300

    17/50

--- solution17/solution17.go
+++ solution17/solution17.go
@@ -1,5 +1,16 @@
 package solution17
 
 func IntsCopy(src []int, maxLen int) []int {
-	return []int{}
+	if maxLen <= 0 {
+		return []int{}
+	}
+	l := 0
+	if maxLen > len(src) {
+		l = len(src)
+	} else {
+		l = maxLen
+	}
+	res := make([]int, l)
+	copy(res, src)
+	return res
 }
---
+++ solution18/solution18.go
@@ -0,0 +1,4 @@
+package solution18
+
+func s18() {
+}

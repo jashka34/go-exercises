f78433296f883f92aac2980dcb9f5f7e612824de
jashka34 <a_cherniadiev@mail.ru>
2025-05-03 02:00:50 +0300

    18/50 begin

--- solution18/solution18.go
+++ solution18/solution18.go
@@ -1,6 +1,8 @@
 package solution18
 
+import "sort"
+
 func UniqueSortedUserIDs(userIDs []int64) []int64 {
-	ret := []int64{}
-	return ret
+	sort.SliceStable(userIDs, func(i, j int) bool { return userIDs[i] < userIDs[j] })
+	return userIDs
 }

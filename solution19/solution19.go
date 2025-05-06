91066c00abd180eea0a1c2d803427a44e27af30f
jashka34 <a_cherniadiev@mail.ru>
2025-05-06 06:55:42 +0300

    19/50 begin

--- solution19/solution19.go
+++ solution19/solution19.go
@@ -1,6 +1,22 @@
 package solution19
 
 func UniqueUserIDs(userIDs []int64) []int64 {
-	var ret []int64
-	return ret
+	if len(userIDs) <= 1 {
+		return userIDs
+	}
+	// sort.SliceStable(userIDs, func(i, j int) bool { return userIDs[i] < userIDs[j] })
+
+	uniq := 0
+	for i := range userIDs {
+		// fmt.Println(i, ":", id, "uniq:", uniq)
+		if i > 0 {
+			if userIDs[i] != userIDs[uniq] {
+				uniq++
+				userIDs[uniq] = userIDs[i]
+				// fmt.Println("userIDs:", userIDs)
+			}
+		}
+	}
+
+	return userIDs[:uniq+1]
 }

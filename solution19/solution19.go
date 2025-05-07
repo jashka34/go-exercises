ea3ddc64a850800c9e36099f91bcf036662f5bcb
jashka34 <a_cherniadiev@mail.ru>
2025-05-07 06:38:16 +0300

    19/50 contitue

--- solution19/solution19.go
+++ solution19/solution19.go
@@ -1,22 +1,30 @@
 package solution19
 
+import "fmt"
+
 func UniqueUserIDs(userIDs []int64) []int64 {
 	if len(userIDs) <= 1 {
 		return userIDs
 	}
 	// sort.SliceStable(userIDs, func(i, j int) bool { return userIDs[i] < userIDs[j] })
 
-	uniq := 0
-	for i := range userIDs {
+	retm := make(map[int64]int, len(userIDs))
+	ret := make([]int64, len(userIDs))
+
+	for i, v := range userIDs {
 		// fmt.Println(i, ":", id, "uniq:", uniq)
-		if i > 0 {
-			if userIDs[i] != userIDs[uniq] {
-				uniq++
-				userIDs[uniq] = userIDs[i]
-				// fmt.Println("userIDs:", userIDs)
-			}
-		}
+		retm[v] = i - 1
 	}
 
-	return userIDs[:uniq+1]
+	for i, v := range retm {
+		fmt.Println("i:", i, "v:", v)
+		ret[v] = i
+	}
+
+	fmt.Println("--------")
+	fmt.Println(userIDs)
+	fmt.Println(retm, "len:", len(retm))
+	fmt.Println(ret)
+	fmt.Println("--------")
+	return ret[:len(retm)]
 }

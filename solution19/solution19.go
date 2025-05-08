864f21ec8daa77830aa1c3e84bdeaa8d85bc99a9
jashka34 <a_cherniadiev@mail.ru>
2025-05-08 08:29:29 +0300

    19/50

--- solution19/solution19.go
+++ solution19/solution19.go
@@ -3,28 +3,22 @@ package solution19
 import "fmt"
 
 func UniqueUserIDs(userIDs []int64) []int64 {
-	if len(userIDs) <= 1 {
-		return userIDs
-	}
-	// sort.SliceStable(userIDs, func(i, j int) bool { return userIDs[i] < userIDs[j] })
+	uniqs := make(map[int64]int, len(userIDs))
+	ret := make([]int64, 0)
 
-	retm := make(map[int64]int, len(userIDs))
-	ret := make([]int64, len(userIDs))
-
-	for i, v := range userIDs {
+	for _, v := range userIDs {
 		// fmt.Println(i, ":", id, "uniq:", uniq)
-		retm[v] = i - 1
-	}
-
-	for i, v := range retm {
-		fmt.Println("i:", i, "v:", v)
-		ret[v] = i
+		_, exists := uniqs[v]
+		if !exists {
+			uniqs[v] = 0
+			ret = append(ret, v)
+		}
 	}
 
 	fmt.Println("--------")
 	fmt.Println(userIDs)
-	fmt.Println(retm, "len:", len(retm))
+	fmt.Println(uniqs, "len:", len(uniqs))
 	fmt.Println(ret)
 	fmt.Println("--------")
-	return ret[:len(retm)]
+	return ret
 }
--- solution19/solution19_test.go
+++ solution19/solution19_test.go
@@ -12,5 +12,5 @@ func TestUniqueUserIDs(t *testing.T) {
 	a.Equal([]int64{10}, UniqueUserIDs([]int64{10}))
 	a.Equal([]int64{55}, UniqueUserIDs([]int64{55, 55}))
 	a.Equal([]int64{55, 33, 22}, UniqueUserIDs([]int64{55, 55, 33, 22}))
-	// a.Equal([]int64{55, 2, 88, 33, 103}, UniqueUserIDs([]int64{55, 2, 88, 33, 2, 2, 55, 103, 33, 88}))
+	a.Equal([]int64{55, 2, 88, 33, 103}, UniqueUserIDs([]int64{55, 2, 88, 33, 2, 2, 55, 103, 33, 88}))
 }

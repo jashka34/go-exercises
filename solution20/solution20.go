5974f201ddf3844a5942056c3f5fc6c8652ba83d
jashka34 <a_cherniadiev@mail.ru>
2025-05-10 13:16:38 +0300

    20/50 continue1

--- solution20/solution20.go
+++ solution20/solution20.go
@@ -1,17 +1,25 @@
 package solution20
 
+import "fmt"
+
 func MostPopularWord(words []string) string {
-	var ret string
+	// var ret string
 	wm := make(map[string]int)
+	max := 0
+	var mword string
 	for _, w := range words {
 		wm[w] = wm[w] + 1
-	}
-	max := 0
-	for w, val := range wm {
-		if val > max {
-			max = val
-			ret = w
+		if wm[w] > max {
+			max = wm[w]
+			mword = w
+			fmt.Println("w:", w, "max:", max, "mword:", mword)
 		}
 	}
-	return ret
+	// for w, val := range wm {
+	// 	if val > max {
+	// 		max = val
+	// 		ret = w
+	// 	}
+	// }
+	return mword
 }
--- solution20/solution20_test.go
+++ solution20/solution20_test.go
@@ -11,6 +11,6 @@ func TestMostPopularWord(t *testing.T) {
 	// a.Equal("hello", MostPopularWord([]string{"hello", "world", "hello"}))
 	// a.Equal("world", MostPopularWord([]string{"hello", "world", "hello", "world", "world"}))
 	// a.Equal("one", MostPopularWord([]string{"one", "two", "three", "four", "five"}))
-	a.Equal("c", MostPopularWord([]string{"a", "b", "c", "c", "d", "e", "e", "d"}))
-	// a.Equal("a", MostPopularWord([]string{"a", "b", "b", "a"}))
+	// a.Equal("c", MostPopularWord([]string{"a", "b", "c", "c", "d", "e", "e", "d"}))
+	a.Equal("a", MostPopularWord([]string{"a", "b", "b", "a"}))
 }

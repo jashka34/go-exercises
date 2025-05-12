589cd25852d42a2fdc8185d694d148ef2328d2cb
jashka34 <a_cherniadiev@mail.ru>
2025-05-12 06:16:42 +0300

    21/50

---
+++ solution21/solution21.go
@@ -0,0 +1,9 @@
+package solution21
+
+func nextASCII(b byte) byte {
+	return 'a'
+}
+
+func prevASCII(b byte) byte {
+	return 'a'
+}
---
+++ solution21/solution21_test.go
@@ -0,0 +1,21 @@
+package solution21
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNextASCII(t *testing.T) {
+	a := assert.New(t)
+	a.Equal(byte('b'), nextASCII(byte('a')))
+	a.Equal(byte('y'), nextASCII(byte('x')))
+	a.Equal(byte('6'), nextASCII(byte('5')))
+}
+
+func TestPrevASCII(t *testing.T) {
+	a := assert.New(t)
+	a.Equal(byte('7'), prevASCII(byte('8')))
+	a.Equal(byte('c'), prevASCII(byte('d')))
+	a.Equal(byte('m'), prevASCII(byte('n')))
+}

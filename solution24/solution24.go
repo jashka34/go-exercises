8cf1b6f4b9b81e411a2070c43b74820a55c92d71
jashka34 <a_cherniadiev@mail.ru>
2025-05-16 02:25:09 +0300

    24/50

--- solution24/solution24.go
+++ solution24/solution24.go
@@ -1,6 +1,16 @@
 package solution24
 
+import (
+	"strings"
+	"unicode"
+)
+
 func latinLetters(s string) string {
-	var ret string
-	return ret
+	ret := &strings.Builder{}
+	for _, w := range s {
+		if unicode.Is(unicode.Latin, w) {
+			ret.WriteRune(w)
+		}
+	}
+	return ret.String()
 }

0b0823a766f1dbe7557ab0841173c784a58eee92
jashka34 <a_cherniadiev@mail.ru>
2025-05-15 06:06:34 +0300

    23/50

--- solution23/solution23.go
+++ solution23/solution23.go
@@ -1,5 +1,17 @@
 package solution23
 
+import (
+	"unicode"
+)
+
 func isASCII(s string) bool {
+	// fmt.Println(s, unicode.MaxASCII)
+	for _, ch := range s {
+		// fmt.Println(s[i], ch)
+		if ch > unicode.MaxASCII {
+			// fmt.Println("false")
+			return false
+		}
+	}
 	return true
 }

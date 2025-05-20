6c8c845d51a5096e0cdf68e4194f2d4190f88c56
jashka34 <a_cherniadiev@mail.ru>
2025-05-20 20:17:03 +0300

    30/50 begin

--- solution30/solution30.go
+++ solution30/solution30.go
@@ -1,4 +1,21 @@
 package solution30
 
-func f() {
+import "errors"
+
+type CreateUserRequest struct {
+	Email                string `json:"email"`
+	Password             string `json:"password"`
+	PasswordConfirmation string `json:"password_confirmation"`
+}
+
+var (
+	errEmailRequired                = errors.New("email is required")
+	errPasswordRequired             = errors.New("password is required")
+	errPasswordConfirmationRequired = errors.New("password confirmation is required")
+	errPasswordDoesNotMatch         = errors.New("password does not match with the confirmation")
+)
+
+func DecodeAndValidateRequest(requestBody []byte) (CreateUserRequest, error) {
+	var cur CreateUserRequest
+	return cur, nil
 }
--- solution30/solution30_test.go
+++ solution30/solution30_test.go
@@ -1,1 +1,47 @@
 package solution30
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDecodeAndValidateRequest(t *testing.T) {
+	a := assert.New(t)
+
+	req, err := DecodeAndValidateRequest(nil)
+	a.Error(err)
+	a.Equal(CreateUserRequest{}, req)
+
+	req, err = DecodeAndValidateRequest([]byte(""))
+	a.Error(err)
+	a.Equal(CreateUserRequest{}, req)
+
+	req, err = DecodeAndValidateRequest([]byte("{}"))
+	a.Error(err)
+	a.Equal(CreateUserRequest{}, req)
+
+	req, err = DecodeAndValidateRequest([]byte("{\"email\":\"\",\"password\":\"test\",\"password_confirmation\":\"test\"}"))
+	a.Equal(errEmailRequired, err)
+	a.Equal(CreateUserRequest{}, req)
+
+	req, err = DecodeAndValidateRequest([]byte("{\"email\":\"test\",\"password\":\"\",\"password_confirmation\":\"test\"}"))
+	a.Equal(errPasswordRequired, err)
+	a.Equal(CreateUserRequest{}, req)
+
+	req, err = DecodeAndValidateRequest([]byte("{\"email\":\"test\",\"password\":\"test\",\"password_confirmation\":\"\"}"))
+	a.Equal(errPasswordConfirmationRequired, err)
+	a.Equal(CreateUserRequest{}, req)
+
+	req, err = DecodeAndValidateRequest([]byte("{\"email\":\"test\",\"password\":\"test\",\"password_confirmation\":\"test2\"}"))
+	a.Equal(errPasswordDoesNotMatch, err)
+	a.Equal(CreateUserRequest{}, req)
+
+	req, err = DecodeAndValidateRequest([]byte("{\"email\":\"email@test.com\",\"password\":\"passwordtest\",\"password_confirmation\":\"passwordtest\"}"))
+	a.NoError(err)
+	a.Equal(CreateUserRequest{
+		Email:                "email@test.com",
+		Password:             "passwordtest",
+		PasswordConfirmation: "passwordtest",
+	}, req)
+}

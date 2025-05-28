1c60f987aa286f930bc384fc8f063ed135d9f2ab
jashka34 <a_cherniadiev@mail.ru>
2025-05-28 06:10:42 +0300

    32/50 continue

---
+++ solution32/solution32.go
@@ -0,0 +1,21 @@
+package solution32
+
+import "errors"
+
+// MergeDictsJob is a job to merge dictionaries into a single dictionary.
+type MergeDictsJob struct {
+	Dicts      []map[string]string
+	Merged     map[string]string
+	IsFinished bool
+}
+
+// errors
+var (
+	errNotEnoughDicts = errors.New("at least 2 dictionaries are required")
+	errNilDict        = errors.New("nil dictionary")
+)
+
+func ExecuteMergeDictsJob(job *MergeDictsJob) (*MergeDictsJob, error) {
+	var ret MergeDictsJob
+	return &ret, nil
+}
---
+++ solution32/solution32_test.go
@@ -0,0 +1,42 @@
+package solution32
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExecuteMergeDictsJob(t *testing.T) {
+	a := assert.New(t)
+
+	job, err := ExecuteMergeDictsJob(&MergeDictsJob{})
+	a.Equal(errNotEnoughDicts, err)
+	a.Equal(true, job.IsFinished)
+
+	job, err = ExecuteMergeDictsJob(&MergeDictsJob{Dicts: []map[string]string{
+		{"a": "b"},
+	}})
+	a.Equal(errNotEnoughDicts, err)
+	a.Equal(true, job.IsFinished)
+
+	job, err = ExecuteMergeDictsJob(&MergeDictsJob{Dicts: []map[string]string{
+		{"a": "b"},
+		nil,
+	}})
+	a.Equal(errNilDict, err)
+	a.Equal(true, job.IsFinished)
+
+	job, err = ExecuteMergeDictsJob(&MergeDictsJob{Dicts: []map[string]string{
+		{"a": "b", "b": "c"},
+		{"d": "e", "f": "g"},
+		{"a": "z", "f": "g"},
+	}})
+	a.NoError(err)
+	a.Equal(true, job.IsFinished)
+	a.Equal(map[string]string{
+		"b": "c",
+		"d": "e",
+		"a": "z",
+		"f": "g",
+	}, job.Merged)
+}

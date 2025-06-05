07ce092f819e1396e86b9658fe42c58db3a9f868
jashka34 <a_cherniadiev@mail.ru>
2025-06-05 18:54:39 +0300

    34/50 begin

--- solution34/solution34.go
+++ solution34/solution34.go
@@ -1,6 +1,21 @@
 package solution34
 
+import "fmt"
+
+func sum(nums []int, ch chan<- int) {
+	var r int
+	for i := range nums {
+		fmt.Println(nums[i])
+		r += nums[i]
+	}
+	ch <- r
+}
+
 func MaxSum(nums1, nums2 []int) []int {
 	var ret []int
+	ch1 := make(chan int)
+	go sum(nums1, ch1)
+	r1 := <-ch1
+	fmt.Println("r1:", r1)
 	return ret
 }

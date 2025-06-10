ceea680b6e6ad3a661531b01885db68b2bc34fb7
jashka34 <a_cherniadiev@mail.ru>
2025-06-10 19:40:21 +0300

    35/50

--- solution35/solution35.go
+++ solution35/solution35.go
@@ -1,13 +1,19 @@
 package solution35
 
+func sum1(nums []int) int {
+	var sum int
+	for _, i := range nums {
+		// fmt.Println(i, sum)
+		sum += i
+	}
+	return sum
+}
+
 func SumWorker(numsCh chan []int, sumCh chan int) {
 	// fmt.Println("start")
-	var sum int
 	for nums := range numsCh {
-		for _, i := range nums {
-			// fmt.Println(i, sum)
-			sum += i
-		}
+		// fmt.Println("nums:", nums)
+		sumCh <- sum1(nums)
+		// fmt.Println("sum:", sum)
 	}
-	sumCh <- sum
 }
